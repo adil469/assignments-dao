5c3f3d24f6bb2a8014217b6d94446763a6700787
adil469 <150043307+adil469@users.noreply.github.com>
2023-11-07 12:29:44 +0500

    q2

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-//Personal Message: Store a person’s name in a variable, and print a message to that person. 
-//Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-let message="Hello Farhan, would you like to learn some typescript today?"
-console.log(message)
+//Name Cases: Store a person’s name in a variable, 
+//and then print that person’s name in lowercase, uppercase, and titlecase.
+let personname = "Adil"
+console.log(`Hello ${personname}, would you like to learn some Python today?`);
