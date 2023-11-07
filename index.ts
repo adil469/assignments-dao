ab9315a0d5a37e7e5b9136ce297247dc2f6ea518
adil469 <150043307+adil469@users.noreply.github.com>
2023-11-07 12:28:09 +0500

    q1

---
+++ index.ts
@@ -0,0 +1,4 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person. 
+//Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let message="Hello Farhan, would you like to learn some typescript today?"
+console.log(message)
